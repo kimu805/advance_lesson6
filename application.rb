d5aa8456cb7d1d11ca58e200bb8959e28ef79912
kimu805 <kimukimuok805@gmail.com>
2024-12-02 12:18:12 +0900

    Update application.rb

--- application.rb
+++ application.rb
@@ -6,3 +6,8 @@ user = User.new({ name: "Tanaka"})
 
 form = TweetForm.new
 form.input(user, { text: "テスト投稿", image: "テスト画像のURL"})
+first_tweet = form.create
+
+puts first_tweet.poster_name
+puts first_tweet.text
+puts first_tweet.image
